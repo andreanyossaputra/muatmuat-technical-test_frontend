913c142aa9171fb8d5650c9ecef250e4d03db88b
Andrean Yos <yosandresaputra07@gmail.com>
2024-10-19 14:04:50 +0700

    second commit

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { deleteProduct } from "../store/productSlice";
 
@@ -6,27 +6,74 @@ const ProductList = ({setEditProduct}) => {
     const products = useSelector((state) => state.products.products);
     const dispatch = useDispatch();
 
+    const [searchTerm, setSearchTerm] = useState("");
+
     const handleDelete = (id) => {
         if (window.confirm('Apakah anda yakin ingin menghapus product berikut?')) {
             dispatch(deleteProduct(id));
         }
     }
 
+    const filteredProducts = products.filter((product) =>
+        product.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-1 gap-4 my-5">
-        {products.map((product, index) => (
-            <div key={index} className="bg-white shadow-lg rounded-lg shadow-md p-6 hover:shadow-x1 transition duration-300">
-                <h2 className="text-x1 font-bold">{product.name}</h2>
-                <p>Harga: {product.price}</p>
-                <p>Stok: {product.stock}</p>
-                <div className="mt-4">
-                    <button onClick={() => handleDelete(product.id)} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
-                    <button onClick={() => setEditProduct(product)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-2 rounded">Edit</button>
-                </div>
+          <div className="mb-3 xl:w-96">
+            <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+              <input
+                type="search"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className="relative m-0 block flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="button-addon2"
+              />
+              <span
+                className="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200"
+                id="basic-addon2"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
             </div>
-        ))}
+          </div>
+          {filteredProducts.map((product, index) => (
+            <div key={index} className="bg-white shadow-lg rounded-lg shadow-md p-6 hover:shadow-x1 transition duration-300">
+              <h2 className="text-x1 font-bold">{product.name}</h2>
+              <p>Harga: {product.price}</p>
+              <p>Stok: {product.stock}</p>
+              <div className="mt-4">
+                <button
+                  onClick={() => handleDelete(product.id)}
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Delete
+                </button>
+                <button
+                  onClick={() => setEditProduct(product)}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-2 rounded"
+                >
+                  Edit
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
-    );
+      );
 }
 
 
