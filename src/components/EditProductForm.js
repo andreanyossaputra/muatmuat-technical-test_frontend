242a7b4db532dd672530c9a2b3d79ae0a5ea247b
Andrean Yos <yosandresaputra07@gmail.com>
2024-10-19 13:15:50 +0700

    second commit

--- src/components/EditProductForm.js
+++ src/components/EditProductForm.js
@@ -40,7 +40,7 @@ const EditProductForm = ({productToEdit, setEditProduct}) => {
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                   type="text"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -57,7 +57,7 @@ const EditProductForm = ({productToEdit, setEditProduct}) => {
                   value={price}
                   onChange={(e) => setPrice(e.target.value)}
                   type="number"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -74,7 +74,7 @@ const EditProductForm = ({productToEdit, setEditProduct}) => {
                   value={stock}
                   onChange={(e) => setStock(e.target.value)}
                   type="number"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -25,7 +25,6 @@ const ProductList = ({setEditProduct}) => {
                 </div>
             </div>
         ))}
-
         </div>
     );
 }
